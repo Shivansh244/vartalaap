f0ac93e03a90d6509eed071932af38e4a7be08ee
unknown <shivanshtomar@gmail.com>
2023-01-11 02:55:54 +0530

    sign-in and sign-up pages done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,48 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
         title: "User Profile"
     });
+}
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Vartalaap | SIGN UP"
+    });
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Vartalaap | SIGN IN"
+    });
+}
+
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user for signing up');
+            return;
+        }
+        if(!user){
+            User.create(req.body, function(err,user){
+                if(err){
+                    console.log('Error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession=function(req,res){
+    //
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
+router.post('/create', usersController.create);
 
 module.exports = router;
