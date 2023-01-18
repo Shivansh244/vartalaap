c2bcb576140a34feb75101ff2b0524587cb36830
unknown <shivanshtomar@gmail.com>
2023-01-19 01:00:35 +0530

    SASS completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,12 +7,20 @@ module.exports.profile = function(req,res){
 }
 
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up',{
         title: "Vartalaap | SIGN UP"
     });
 }
 
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: "Vartalaap | SIGN IN"
     });
@@ -43,6 +51,14 @@ module.exports.create = function(req,res){
     })
 }
 
+//sign in and create a session for the user
 module.exports.createSession=function(req,res){
-    //
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout(req.user, err => {
+        if(err) return next(err);
+        return res.redirect("/");
+      });
 }
--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assests/scss',
+    dest: './assests/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
 
 app.use(express.urlencoded());
 
@@ -16,12 +29,39 @@ app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
 app.use(expressLayouts);
-//USE Express Router 
-app.use('/',require('./routes'));
+
 
 // setup view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//mongo store is used to store session cookie in the db
+app.use(session({
+    name: 'vartalaap',
+    secret: 'alookandha',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 *60*100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection : db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser)
+
+//USE Express Router 
+app.use('/',require('./routes'));
 
 app.listen(port, function(err){
     if(err){
