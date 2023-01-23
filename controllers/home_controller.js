6adcc1b44f20f51715f3ae6daca527087b51ccde
unknown <shivanshtomar@gmail.com>
2023-01-23 19:38:35 +0530

    Comments added and displayed on HOME

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,29 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 24);
-    return res.render('home',{
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 24);
+    // Post.find({}, function(err,posts){
+    //     return res.render('home',{
+    //         title: "Vartalaap | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
+        return res.render('home',{
+            title: "Home",
+            posts: posts
+        });
     });
+    
 }
